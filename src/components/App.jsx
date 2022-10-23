968d08d7c1a0bc9c1105a863a0ef3342091d4393
Victiriia Ripka <98872709+Victoriia-Ripka@users.noreply.github.com>
2022-10-23 15:39:06 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,59 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Searchbar } from './Searchbar';
 import ImageGallery from './ImageGallery';
-import { ToastContainer } from 'react-toastify';
+import { AppSection, ButtonWrapper } from './styles.styled';
+import { fetchImages } from 'api';
+import { Button } from './Button';
+import { Loader } from './Loader';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { AppSection } from './styles.styled';
 
 export default function App() {
   const [input, setInput] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState('');
 
   const handleSubmit = e => {
     setInput(e);
+    setPage(1);
+  };
+
+  useEffect(() => {
+    if (input === '') {
+      return;
+    }
+    setLoading('true');
+    fetchImages(input, page)
+      .then(response => {
+        if (response.length > 0) {
+          page > 1
+            ? setImages(prev => [...prev, ...response])
+            : setImages([...response]);
+        } else {
+          toast.error('Wrong request');
+        }
+        setLoading('');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [input, page]);
+
+  const loadMore = () => {
+    setPage(page => page + 1);
   };
 
   return (
     <AppSection>
       <Searchbar onSubmit={handleSubmit} />
-      
+      {loading && <Loader />}
       <ImageGallery
-        input={input}
+        images={images}
       />
+      <ButtonWrapper>
+        {images.length >= 12 && <Button loadMore={loadMore} />}
+      </ButtonWrapper>
       <ToastContainer autoClose={3000} />
     </AppSection>
   );
--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,50 +1,13 @@
-import { useState, useEffect } from 'react';
 import { ImageGalleryItem } from './ImageGalleryItem';
-import { Button } from './Button';
-import { Gallery, Image, ButtonWrapper } from './styles.styled';
-import { toast } from 'react-toastify';
-import { fetchImages } from 'api';
+import { Gallery, Image } from './styles.styled';
 import { PropTypes } from 'prop-types';
-import { Loader } from './Loader';
-import { nanoid } from 'nanoid'
 
-export default function ImageGallery({ input }) {
-  const [page, setPage] = useState(1);
-  const [images, setImages] = useState([]);
-  const [loading, setLoading] = useState('');
+import { nanoid } from 'nanoid';
 
-  useEffect(() => {
-    if (input === '') {
-      return;
-    }
-    setLoading('true');
-    fetchImages(input, page)
-      .then(response => {
-        if (response.length > 0) {
-          if( page === 1) {
-            setImages([])
-            setImages([...response])
-          }
-          page > 1
-            ? setImages(prev => [...prev, ...response])
-            : setImages([...response]);
-        } else {
-          toast.error('Wrong request');
-        }
-        setLoading('');
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }, [input, page]);
-
-  const loadMore = () => {
-    setPage(page => page + 1);
-  };
+export default function ImageGallery({ images }) {
 
   return (
     <>
-      {loading && <Loader />}
       <Gallery>
         {images.map(image => (
           <Image key={nanoid()}>
@@ -52,14 +15,10 @@ export default function ImageGallery({ input }) {
           </Image>
         ))}
       </Gallery>
-
-      <ButtonWrapper>
-        {images.length > 0 && <Button loadMore={loadMore} />}
-      </ButtonWrapper>
     </>
   );
 }
 
 ImageGallery.propTypes = {
-  input: PropTypes.string.isRequired,
+  // images: PropTypes.string.isRequired,
 };
